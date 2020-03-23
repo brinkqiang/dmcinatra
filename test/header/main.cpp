9fcb1d9afc918c8f6548ac161c452150e9f92cfb
brinkqiang <brink.qiang@gmail.com>
2020-03-23 16:23:40 +0800

    add

---
+++ test/header/main.cpp
@@ -0,0 +1,30 @@
+
+#include "cinatra.hpp"
+#include "dmutil.h"
+
+using namespace cinatra;
+
+int main() {
+	DMSetWorkPath(DMGetRootPath() + ".." + PATH_DELIMITER_STR + "www");
+
+	http_server server(std::thread::hardware_concurrency());
+	server.listen("0.0.0.0", "8080");
+	server.set_http_handler<GET, POST>("/test", [](request& req, response& res) {
+		auto name = req.get_header_value("name");
+		if (name.empty()) {
+			res.set_status_and_content(status_type::bad_request, "no name");
+			return;
+		}
+
+		auto id = req.get_query_value("id");
+		if (id.empty()) {
+			res.set_status_and_content(status_type::bad_request);
+			return;
+		}
+
+		res.set_status_and_content(status_type::ok, "hello world");
+	});
+
+	server.run();
+	return 0;
+}
