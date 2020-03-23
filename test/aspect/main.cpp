a1bec6194dee590bdf358878cd608a19d4b54950
brinkqiang <brink.qiang@gmail.com>
2020-03-23 16:22:53 +0800

    add

---
+++ test/aspect/main.cpp
@@ -0,0 +1,49 @@
+#include "cinatra.hpp"
+#include "dmutil.h"
+
+using namespace cinatra;
+
+//日志切面
+struct log_t
+{
+	bool before(request& req, response& res) {
+		std::cout << "before log" << std::endl;
+		return true;
+	}
+
+	bool after(request& req, response& res) {
+		std::cout << "after log" << std::endl;
+		return true;
+	}
+};
+
+//校验的切面
+struct check {
+	bool before(request& req, response& res) {
+		std::cout << "before check" << std::endl;
+		if (req.get_header_value("name").empty()) {
+			res.set_status_and_content(status_type::bad_request);
+			return false;
+		}
+		
+		return true;
+	}
+
+	bool after(request& req, response& res) {
+		std::cout << "after check" << std::endl;
+		return true;
+	}
+};
+
+int main() {
+	DMSetWorkPath(DMGetRootPath() + ".." + PATH_DELIMITER_STR + "www");
+
+	http_server server(std::thread::hardware_concurrency());
+	server.listen("0.0.0.0", "8080");
+	server.set_http_handler<GET, POST>("/aspect", [](request& req, response& res) {
+		res.set_status_and_content(status_type::ok, "hello world");
+	}, check{}, log_t{});
+
+	server.run();
+	return 0;
+}
