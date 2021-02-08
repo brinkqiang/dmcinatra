7be37920405fff26b47daa18b8f19fdbb6931b04
brinkqiang <brink.qiang@gmail.com>
2021-02-09 00:34:17 +0800

    add

--- build.sh
+++ build.sh
@@ -10,13 +10,6 @@
 # libtoolize && aclocal && autoheader && autoconf && automake --add-missing
 # sh configure
 # popd
-if [ -f /etc/redhat-release ]; then
-    sudo yum -y install uuid-devel
-fi
-
-if [ -f /etc/lsb-release ]; then
-    sudo apt -y install uuid-dev
-fi
 
 rm -rf build
 mkdir -p build
