aa6f58fe4b46e056a3d3e14357af0e0bde70a6a2
brinkqiang <brink.qiang@gmail.com>
2022-03-24 22:43:00 +0800

    add

--- test/download/main.cpp
+++ test/download/main.cpp
@@ -6,14 +6,15 @@ using namespace cinatra;
 
 int main()
 {
-    DMSetWorkPath();
+    DMSetWorkPath(DMGetRootPath() + PATH_DELIMITER_STR + ".." + PATH_DELIMITER_STR);
 
-    std::string uri =
-        "http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx";
 
     {
+        std::string uri =
+            "http://127.0.0.1:8080/Mindjet.zip";
+
         auto client = cinatra::client_factory::instance().new_client();
-        client->download(uri, "test.jpg", [](response_data data)
+        client->download(uri, "www2/Mindjet.zip", [](response_data data)
         {
             if (data.ec)
             {
@@ -26,46 +27,42 @@ int main()
     }
 
     {
+        std::string uri =
+            "http://127.0.0.1:8080/Mind jet.zip";
+
         auto client = cinatra::client_factory::instance().new_client();
-        client->download(uri, [](auto ec, auto data)
+        client->download(uri, "www2/Mind jet.zip", [](response_data data)
         {
-            if (ec)
+            if (data.ec)
             {
-                std::cout << ec.message() << "\n";
+                std::cout << data.ec.message() << "\n";
                 return;
             }
 
-            if (data.empty())
-            {
-                std::cout << "finished all \n";
-            }
-            else
-            {
-                std::cout << data.size() << "\n";
-            }
+            std::cout << "finished download\n";
         });
     }
 
-    {
-        auto client = cinatra::client_factory::instance().new_client();
-        client->download(uri, [](auto ec, auto data)
-        {
-            if (ec)
-            {
-                std::cout << ec.message() << "\n";
-                return;
-            }
+    //{
+    //    auto client = cinatra::client_factory::instance().new_client();
+    //    client->download(uri, [](auto ec, auto data)
+    //    {
+    //        if (ec)
+    //        {
+    //            std::cout << ec.message() << "\n";
+    //            return;
+    //        }
 
-            if (data.empty())
-            {
-                std::cout << "finished all \n";
-            }
-            else
-            {
-                std::cout << data.size() << "\n";
-            }
-        });
-    }
+    //        if (data.empty())
+    //        {
+    //            std::cout << "finished all \n";
+    //        }
+    //        else
+    //        {
+    //            std::cout << data.size() << "\n";
+    //        }
+    //    });
+    //}
 
     while (1)
     {
--- test/helloworld/main.cpp
+++ test/helloworld/main.cpp
@@ -4,14 +4,14 @@
 using namespace cinatra;
 
 int main() {
-    DMSetWorkPath();
+    DMSetWorkPath(DMGetRootPath() + PATH_DELIMITER_STR + ".." + PATH_DELIMITER_STR);
 
-	http_server server(std::thread::hardware_concurrency());
-	server.listen("0.0.0.0", "8080");
-	server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
-		res.set_status_and_content(status_type::ok, "hello world");
-	});
+    http_server server(std::thread::hardware_concurrency());
+    server.listen("0.0.0.0", "8080");
+    server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
+        res.set_status_and_content(status_type::ok, "hello world");
+    });
 
-	server.run();
-	return 0;
+    server.run();
+    return 0;
 }
