f968e6ddd840826b2a508ac5301f86278742a5cd
brinkqiang <brink.qiang@gmail.com>
2022-07-25 17:40:46 +0800

    add

---
+++ test/dmcinatraclient/main.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "cinatra.hpp"
+#include "dmutil.h"
+
+using namespace cinatra;
+
+void print(const response_data& result)
+{
+    print(result.ec, result.status, result.resp_body, result.resp_headers.second);
+}
+
+int main()
+{
+    DMSetWorkPath();
+
+    auto client = cinatra::client_factory::instance().new_client();
+    std::string uri = "https://api.weixin.qq.com/sns/jscode2session";
+    {
+        response_data result = client->get(uri);
+        print(result);
+    }
+
+    return 0;
+}
